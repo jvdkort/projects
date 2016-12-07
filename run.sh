d6d69061ff84d4ed29ba013ddecd6dd96db944fe
jvdkort <jaspervanderkort@gmail.com>
2016-12-07 15:24:48 +0000

    Delete run.sh

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Starting scraper"
-scrapy runspider mycinema.py -t json --nolog -o - > "movies.json"
-echo "Scrape complete, checking movies with imdb"
-python3 checkimdb.py movies.json
